36fee0c45a1e472e56a60846e78daafdb8e50e8f
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-05-21 15:24:06 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- printf.c
+++ printf.c
@@ -9,15 +9,47 @@ int _printf(const char *format, ...)
 {
 	va_list arg;
 	int cont = 0;
-	int i = 0;
+	int i = 0, k = 0;
+	char *str = NULL;
 
 	va_start(arg, format);
 
 	while (format[i] != '\0')
 	{
-	  if (_putchar(format[i]) != -1)
-	    cont += 1;
-	  
+	  if (format[i]) != '%')
+	  {
+	    _putchar(format[i]);
+	    cont++;
+	  }
+	  else
+	  {
+	    if (format[i+1] == 'c')
+	    {
+	      _putchar(va_arg(arg, int));
+	      cont++;
+	      i++;
+	    }
+	    else if (format[i+1] == 's')
+	    {
+	      i++;
+	      str = va_arg(arg, char *);
+	      k = 0;
+	      
+	      while (str[k] != '\0')
+	      {
+		_putchar(str[k]);
+		cont++;
+		k++;
+	      }
+	    }
+	    else if (format[i+1] == '%')
+	    {
+	      i++;
+	      _putchar('%');
+	      cont++;
+	    }
+	  }
+	
 	  i++;
 	}
 
